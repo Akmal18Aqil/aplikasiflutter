4ad8b9e4f4c4b717b8398a8cff3e5822b846f75d
Akmal18Aqil <akmalaqilwahyu@gmail.com>
2022-08-09 23:48:33 +0700

    Update 2

---
+++ materi-2/materi.dart
@@ -0,0 +1,9 @@
+// void main() {
+//   print("Hello World2");
+// }
+void main(List<String> args) {
+  var sifat = "Rajin";
+
+  print(sifat != "Rajin");
+  print(sifat != "Malas");
+}
---
+++ materi-2/type.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  // Tugas String Nomor 1
+// var word = 'Dart ';
+// var second = 'is ';
+// var third = 'awesome ';
+// var fourth = 'and ';
+// var fifth = 'I ';
+// var sixth = 'love ';
+// var seventh = 'it!';
+
+// print(word+second+third+fourth+fifth+sixth+seventh);
+
+  // Tugas String Nomor 2
+  // var tugas = "I am going to be Flutter Developer";
+
+  // var pertama = tugas[0];
+  // var kedua = tugas[2]+tugas[3];
+  // var ketiga = tugas[5]+tugas[6]+tugas[7]+tugas[8]+tugas[9];
+  // var keempat = tugas[11]+tugas[12];
+  // var kelima = tugas[14]+tugas[15];
+  // var keenam = tugas[17]+tugas[18]+tugas[19]+tugas[20]+tugas[21]+tugas[22]+tugas[23];
+  // var ketujuh = tugas[25]+tugas[26]+tugas[27]+tugas[28]+tugas[29]+tugas[30]+tugas[31]+tugas[32]+tugas[33];
+
+  // print('First Word: ' + pertama);
+  // print('Second Word: ' + kedua);
+  // print('Third Word: ' + ketiga);
+  // print('Fourth Word: ' + keempat);
+  // print('Fifth Word: ' + kelima);
+  // print('Sixth Word: ' + keenam);
+  // print('Seventh Word: ' + ketujuh);
+
+  // Tugas String Nomor 3
+  print("Masukkan Nama Depan: ");
+  String? depan = stdin.readLineSync();
+  print("Masukkan Nama Belakang: ");
+  String? belakang = stdin.readLineSync();
+
+  print("Nama Lengkap Anda: " + depan! + " " + belakang!);
+
+  // Tugas String Nomor 4
+// var a = 5;
+// var b = 10;
+
+// var kali = a*b;
+// var bagi = a/b;
+// var tambah = a+b;
+// var kurang = a-b;
+
+// print("Perkalian = $kali");
+// print("Pembagian = $bagi");
+// print("Penambahan = $tambah");
+// print("pengurangan = $kurang");
+}
