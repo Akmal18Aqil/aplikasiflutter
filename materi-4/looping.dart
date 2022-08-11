2470edeb5b06dc9b78b0a48ee1aaba89427f1f4b
Akmal18Aqil <akmalaqilwahyu@gmail.com>
2022-08-11 21:57:11 +0700

    Malam Jumat

---
+++ materi-4/looping.dart
@@ -0,0 +1,36 @@
+void main(List<String> args) {
+  // print("Looping Pertama");
+  // var angka = 2;
+  // while (angka <= 20) {
+  //   print(angka.toString() + "- I Love Coding");
+  //   angka += 2;
+  // }
+  // print("Looping Kedua");
+  // var lagi = 20;
+  // while (lagi >= 2) {
+  //   print(lagi.toString() + " - Aku Akan Bangkit");
+  //   lagi -= 2;
+  // }
+
+  // Pisah
+  // for (var bilangan = 1; bilangan <= 20; bilangan++) {
+  //   if (bilangan % 2 == 1) {
+  //     if (bilangan % 3 == 0) {
+  //       print(bilangan.toString() + "- tiga");
+  //     }
+  //     print(bilangan.toString() + "- Santai");
+  //   } else if (bilangan % 2 == 0) {
+  //     print(bilangan.toString() + "- Berlian");
+  //   }
+  // }
+
+  for (var i = 1; i <= 4; i++) {
+    print("#" * 8);
+  }
+
+  // var bangun = 1;
+  // while (bangun <= 7) {
+  //   print("#" * bangun);
+  //   bangun++;
+  // }
+}
